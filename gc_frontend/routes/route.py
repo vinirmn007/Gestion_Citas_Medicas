3567e0b530826005b9ca7acf1b55b9ca446bfecd
Alexis Roman <alexis.roman@unl.edu.ec>
2024-11-03 20:57:12 -0500

    create base project

---
+++ gc_frontend/routes/route.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, json, render_template, request, redirect, url_for, flash
+import requests
+
+router = Blueprint('router', __name__)
+
+@router.route('/')
+def home():
+    return render_template('base.html')
