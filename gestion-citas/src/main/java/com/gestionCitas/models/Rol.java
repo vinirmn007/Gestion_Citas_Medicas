5dddfd18015932750d9918e8f579e260c57d7eb7
Estefania <lisbeth.cale@unl.edu.ec>
2024-12-10 12:01:36 -0500

    cambioa

--- gestion-citas/src/main/java/com/gestionCitas/models/Rol.java
+++ gestion-citas/src/main/java/com/gestionCitas/models/Rol.java
@@ -1,27 +1,30 @@
 package com.gestionCitas.models;
 
 public class Rol {
-    private Integer id;
-    private String nombre;
+  private Integer id;
+  private String nombre;
 
-    public Rol(Integer id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
+  public Rol() {
+  }
+
+  public Rol(Integer id, String nombre) {
+    this.id = id;
+    this.nombre = nombre;
+  }
 
 
+  public Integer getId() {
+    return this.id;
+  }
+  public void setId(Integer value) {
+    this.id = value;
+  }
 
-    public Integer getId() {
-      return this.id;
-    }
-    public void setId(Integer value) {
-      this.id = value;
-    }
-
-    public String getNombre() {
-      return this.nombre;
-    }
-    public void setNombre(String value) {
-      this.nombre = value;
-    }
+  public String getNombre() {
+    return this.nombre;
+  }
+  public void setNombre(String value) {
+    this.nombre = value;
+  }
 }
+
