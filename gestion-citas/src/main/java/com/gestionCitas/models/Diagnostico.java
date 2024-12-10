972b86de145e92df0a8f4d08b9def299de2e91eb
Emilio Jaramillo <emilio.jaramillo@unl.edu.ec>
2024-12-10 11:06:17 -0500

    Udpate

--- gestion-citas/src/main/java/com/gestionCitas/models/Diagnostico.java
+++ gestion-citas/src/main/java/com/gestionCitas/models/Diagnostico.java
@@ -3,10 +3,21 @@ package com.gestionCitas.models;
 public class Diagnostico {
     private int id;
     private String descripcion;
+    private Integer idCitaMedica;
+
+
+    public Integer getIdCitaMedica() {
+        return idCitaMedica;
+    }
+
+    public void setIdCitaMedica(Integer idCitaMedica) {
+        this.idCitaMedica = idCitaMedica;
+    }
     
     public Diagnostico() {
         this.id = 0;
         this.descripcion = "";
+        this.idCitaMedica = null;
     }
 
     public Diagnostico(String descripcion) {
