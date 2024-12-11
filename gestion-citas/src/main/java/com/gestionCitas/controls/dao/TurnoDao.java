e33d64bf65819d8416a8ff49a7a27ba5f95bb719
Boris Rengel <boris.rengel@unl.edu.ec>
2024-12-11 16:15:01 -0500

    turno api

--- gestion-citas/src/main/java/com/gestionCitas/controls/dao/TurnoDao.java
+++ gestion-citas/src/main/java/com/gestionCitas/controls/dao/TurnoDao.java
@@ -28,7 +28,7 @@ public class TurnoDao extends AdapterDao<Turno> {
         this.turno = turno;
     }
 
-    public LinkedList getListAll() {
+    public LinkedList<Turno> getListAll() {
         if (listAll == null) {
             this.listAll = listAll();
         }
