10e5dd71612f4a59f7c0dc8df5e9b260d64495af
Emilio Jaramillo <emilio.jaramillo@unl.edu.ec>
2025-01-03 22:50:29 -0500

    fix error Diagnostico front

--- gc_frontend/routes/diagnosticoRoute.py
+++ gc_frontend/routes/diagnosticoRoute.py
@@ -12,10 +12,10 @@ def citas_medicas():
     return render_template('parts/diagnostico/diagnostico.html', diagnosticos=data)
 
 #PARA VER LOS RECURSOS DEL TEMPLATE
-@citas_route.route('/tablas')
+@diagnostico_route.route('/tablas')
 def tablas():
     return render_template('tables/tables.html')
 
-@citas_route.route('/form_v')
+@diagnostico_route.route('/form_v')
 def form_():
     return render_template('forms/form_validation.html')
